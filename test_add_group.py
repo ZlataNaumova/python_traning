e631c0c402189f5f7ad5a35dbd62e274563cf663
ZlataNaumova <zlata@corp.ifmo.ru>
2018-03-09 13:34:55 +0300

    Выделены вспомогательные методы и классы

---
+++ test_add_group.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from group import Group
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=r'C:\pselenium\geckodriver-v0.19.1-win64\geckodriver.exe')
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="group_test", header="12", footer="112"))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+        #driver.find_element_by_name("user").clear()
+        #driver.find_element_by_name("user").send_keys("admin")
+
+    def test_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, driver, group):
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_name("submit").click()
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text("groups").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
